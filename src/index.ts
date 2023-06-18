64f91dc7fa2921d83dd8e54d67e6ab7565dd063f
VelizarStavrev <velizar.g.stavrev@gmail.com>
2023-06-18 20:57:49 +0300

    Bumping the limit of express JSON requests.

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, authorization');
   next();
 });
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(routes);
 
 app.listen(config.port, () => {
